d78769926db769945ea462ca71c1d89ce24fdce5
JS <realitix@gmail.com>
2019-05-07 13:53:19 +0200

    Switch to amethyst

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-#[macro_use]
-extern crate glium;
+extern crate amethyst;
 
-mod gdx;
-mod app;
+use amethyst::{
+    prelude::*,
+    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    utils::application_root_dir,
+};
 
-use gdx::app::GliumApplicationConfiguration;
-use gdx::app::GliumApplication;
+struct Example;
 
-use app::BaboApplication;
+impl SimpleState for Example {}
 
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
 
-fn main() {
-    let config = GliumApplicationConfiguration{};
-    let mut app = GliumApplication::new(BaboApplication::new(), config);
-    app.run()
+    let path = format!(
+        "{}/resources/display_config.ron",
+        application_root_dir()
+    );
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
+            .with_pass(DrawFlat::<PosNormTex>::new()),
+    );
+
+    let game_data =
+        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
+    let mut game = Application::new("./", Example, game_data)?;
+
+    game.run();
+
+    Ok(())
 }
